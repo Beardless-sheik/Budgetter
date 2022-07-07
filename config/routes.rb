69240c9aedf6021f1c3078f55503b840b6ede6ed
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-07 13:17:20 +0200

    Add basic routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  Rails.application.routes.draw do
+    devise_for :users
+    root to: "categories#index"
+  
+    resources :categories, only: [:index, :new, :create] do
+      resources :transactions, only: [:index, :new, :create] 
+    end
+  end
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
