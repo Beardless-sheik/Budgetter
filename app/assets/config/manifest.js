7608145b7d3f0f334c9a60114fc855f823bcbc9c
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-08 11:40:07 +0200

    Add New Category Static Page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/devise .css
 //= link_directory ../stylesheets/category .css
+//= link_directory ../stylesheets/payment .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 
