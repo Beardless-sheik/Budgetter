05e68bbe6c43f0b2965f378fafd6a09a1d390c66
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-08 21:15:30 +0200

    Add integration tests for log in instructions

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(_resource)
-    redirect_to root_path
+    redirect_to categories_path
   end
 end
