68022054979be9b4b30eb806d576e6d8b268d874
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-08 22:03:49 +0200

    Fix linter errors

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'Tests for the Log in Page', type: :feature do
+RSpec.feature 'Tests Log in Page', type: :feature do
   background { visit new_user_session_path }
   scenario 'if there is form login' do
     expect(page.has_field?('Email')).to be true
@@ -24,13 +24,8 @@ RSpec.feature 'Tests for the Log in Page', type: :feature do
     end
 
     scenario 'if credentials are right' do
-      @user1 = User.create(
-        Name: "Alick Nyirenda",
-        email: "test@test.com",
-        password: "123Alick",
-        confirmed_at: DateTime.now
-      )
-
+      @user1 = User.create(Name: 'Alick Nyirenda', email: 'test@test.com',
+                           password: '123Alick', confirmed_at: DateTime.now)
       within 'form' do
         fill_in 'Email', with: @user1.email
         fill_in 'Password', with: @user1.password
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -14,23 +14,23 @@ describe Category, type: :model do
   describe 'check model method - "check_payment_total" ' do
     before(:each) do
       @user = User.new(
-        :Name                  => "Alick Nyirenda",
-        :email                 => "test@test.com",
-        :password              => "123Alick",
-        :password_confirmation => "123Alick",
-        :confirmed_at => DateTime.now
+        Name: 'Alick Nyirenda',
+        email: 'test@test.com',
+        password: '123Alick',
+        password_confirmation: '123Alick',
+        confirmed_at: DateTime.now
       )
-      @category4 = Category.create(Name: "Food", Icon: "https://img.icons8.com/material-outlined/96/1A1A1A/no-image.png",
-        user: @user)
+      @category4 = Category.create(Name: 'Food', Icon: 'https://img.icons8.com/material-outlined/96/1A1A1A/no-image.png',
+                                   user: @user)
     end
-    
+
     it 'should return zero if no payments are present' do
       expect(@category4.check_payment_total).to eq(0)
     end
 
     it 'should return total of all payments if payments are present' do
-      payment1_category1 = Payment.create(Name: "Mc Donalds", Amount: 50 , user: @user, category_id: @category4.id)
-      payment1_category2 = Payment.create(Name: "Hungry Lion", Amount: 50 , user: @user, category_id: @category4.id)
+      Payment.create(Name: 'Mc Donalds', Amount: 50, user: @user, category_id: @category4.id)
+      Payment.create(Name: 'Hungry Lion', Amount: 50, user: @user, category_id: @category4.id)
       expect(@category4.check_payment_total).to eq(100)
     end
   end
