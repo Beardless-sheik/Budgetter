39e857174fcabc436ef11ae390a95ab882b7646b
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-08 15:23:38 +0200

    Add dynamic clickable links in category index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,20 @@ class CategoriesController < ApplicationController
   end
 
   def new; end
+
+  def create
+    new_category = Category.new(category_params)
+    p new_category
+    if new_category.save
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      render :new, alert: 'Error occurred, please try again. Category not saved'
+    end
+  end
+
+  private
+
+  def category_params
+    params.permit(:Name, :Icon, :user_id).with_defaults(user_id: current_user.id)
+  end
 end
